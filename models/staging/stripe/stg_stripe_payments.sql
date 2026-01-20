9de61459a7868964cebc93090296efaaf6602746
Andrea McGrane <andrea.mcgrane@gmail.com>
2026-01-20 13:34:15 -0600

    Updated payments staging file naming

--- models/staging/stripe/stg_stripe_payments.sql
+++ models/staging/stripe/stg_stripe_payments.sql
@@ -1,7 +1,7 @@
 select
         id as payment_id,
-        orderid,
-        paymentmethod,
+        orderid as order_id,
+        paymentmethod as payment_method,
         status,
         amount / 100 as amount,
         created as created_at,
